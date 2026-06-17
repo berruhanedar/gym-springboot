6e248298e801a53cecf2631900bc3a3ae64e81e2
Berru Hanedar <115636713+berruhanedar@users.noreply.github.com>
2026-06-17 17:26:37 +0300

    Initial commit

---
+++ src/main/java/com/berruhanedar/app/gym_springboot/GymSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.berruhanedar.app.gym_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymSpringbootApplication.class, args);
+	}
+
+}
