f8e6437c8673fc495b68c679e94744ea4e6dd13f
Berru Hanedar <115636713+berruhanedar@users.noreply.github.com>
2026-06-19 01:35:17 +0300

    Implement custom exception for missing entities

---
+++ src/main/java/com/berruhanedar/app/gym_springboot/exception/EntityNotFoundException.java
@@ -0,0 +1,5 @@
+package com.berruhanedar.app.gym_springboot.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException(String message) { super(message); }
+}
