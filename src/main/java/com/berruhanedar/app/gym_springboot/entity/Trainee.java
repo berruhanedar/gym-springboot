b3234dc7c8d476e1a7886473d719593281fe4f86
Berru Hanedar <115636713+berruhanedar@users.noreply.github.com>
2026-06-26 15:37:39 +0300

    Map Trainee entity and relationships

--- src/main/java/com/berruhanedar/app/gym_springboot/entity/Trainee.java
+++ src/main/java/com/berruhanedar/app/gym_springboot/entity/Trainee.java
@@ -1,15 +1,34 @@
 package com.berruhanedar.app.gym_springboot.entity;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+@Entity
+@Table(name = "trainees")
 @Data
 @NoArgsConstructor
-@EqualsAndHashCode(callSuper = true)
+@EqualsAndHashCode(callSuper = true, exclude = {"trainers", "trainings"})
+@ToString(callSuper = true, exclude = {"trainers", "trainings"})
 public class Trainee extends User {
+
+    @Column(name = "date_of_birth")
     private LocalDate dateOfBirth;
+
+    @Column(name = "address")
     private String address;
+
+    @ManyToMany
+    @JoinTable(
+            name = "trainee_trainer",
+            joinColumns = @JoinColumn(name = "trainee_id"),
+            inverseJoinColumns = @JoinColumn(name = "trainer_id")
+    )
+    private Set<Trainer> trainers = new HashSet<>();
+
+    @OneToMany(mappedBy = "trainee", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private Set<Training> trainings = new HashSet<>();
 }
