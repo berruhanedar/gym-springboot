2aaa044ed6dad3f1d7b06f29d92e4a3fdf132d68
Berru Hanedar <115636713+berruhanedar@users.noreply.github.com>
2026-06-26 14:04:19 +0300

    Add Java-based Spring Core configuration

---
+++ src/main/java/com/berruhanedar/app/gym_springboot/config/AppConfig.java
@@ -0,0 +1,18 @@
+package com.berruhanedar.app.gym_springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@ComponentScan(basePackages = "com.berruhanedar.app.gym_springboot")
+@PropertySource("classpath:application.properties")
+public class AppConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
