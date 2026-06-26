8494370b4cdd31258214a43e93cf1cb74bddc071
Berru Hanedar <115636713+berruhanedar@users.noreply.github.com>
2026-06-27 00:25:52 +0300

    Replace authentication parameters with CredentialsDTO

--- src/main/java/com/berruhanedar/app/gym_springboot/service/AuthenticationService.java
+++ src/main/java/com/berruhanedar/app/gym_springboot/service/AuthenticationService.java
@@ -2,6 +2,7 @@ package com.berruhanedar.app.gym_springboot.service;
 
 import com.berruhanedar.app.gym_springboot.dao.TraineeDao;
 import com.berruhanedar.app.gym_springboot.dao.TrainerDao;
+import com.berruhanedar.app.gym_springboot.dto.CredentialsDTO;
 import com.berruhanedar.app.gym_springboot.exception.AuthenticationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,16 +25,18 @@ public class AuthenticationService {
     }
 
     @Transactional(readOnly = true)
-    public void authenticateTrainee(String username, String password) {
-        traineeDao.findByUsername(username)
-                .filter(trainee -> trainee.getPassword().equals(password))
-                .orElseThrow(() -> new AuthenticationException("Invalid trainee username or password."));
+    public void authenticateTrainee(CredentialsDTO credentials) {
+        traineeDao.findByUsername(credentials.getUsername())
+                .filter(trainee -> trainee.getPassword().equals(credentials.getPassword()))
+                .orElseThrow(() ->
+                        new AuthenticationException("Invalid trainee username or password."));
     }
 
     @Transactional(readOnly = true)
-    public void authenticateTrainer(String username, String password) {
-        trainerDao.findByUsername(username)
-                .filter(trainer -> trainer.getPassword().equals(password))
-                .orElseThrow(() -> new AuthenticationException("Invalid trainer username or password."));
+    public void authenticateTrainer(CredentialsDTO credentials) {
+        trainerDao.findByUsername(credentials.getUsername())
+                .filter(trainer -> trainer.getPassword().equals(credentials.getPassword()))
+                .orElseThrow(() ->
+                        new AuthenticationException("Invalid trainer username or password."));
     }
 }
