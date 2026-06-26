dc6db18cfca2ca0fe60f70c3694cb4af7814e65d
Berru Hanedar <115636713+berruhanedar@users.noreply.github.com>
2026-06-26 15:47:50 +0300

    Convert TrainingType to JPA entity

--- src/main/java/com/berruhanedar/app/gym_springboot/entity/TrainingType.java
+++ src/main/java/com/berruhanedar/app/gym_springboot/entity/TrainingType.java
@@ -1,13 +1,31 @@
 package com.berruhanedar.app.gym_springboot.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
-@Data
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "training_types")
+@EqualsAndHashCode(exclude = {"trainings", "trainers"})
+@ToString(exclude = {"trainings", "trainers"})
 public class TrainingType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "training_type_name", nullable = false, unique = true)
     private String trainingTypeName;
+
+    @OneToMany(mappedBy = "trainingType")
+    private Set<Training> trainings = new HashSet<>();
+
+    @OneToMany(mappedBy = "specialization")
+    private Set<Trainer> trainers = new HashSet<>();
 }
