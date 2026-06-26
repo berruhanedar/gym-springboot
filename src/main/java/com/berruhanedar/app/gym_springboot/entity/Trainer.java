a0b8c7f59dd83ed56974d68b0f42ce2c6c3831d2
Berru Hanedar <115636713+berruhanedar@users.noreply.github.com>
2026-06-26 15:39:40 +0300

    Add JPA mappings to Trainer entity

--- src/main/java/com/berruhanedar/app/gym_springboot/entity/Trainer.java
+++ src/main/java/com/berruhanedar/app/gym_springboot/entity/Trainer.java
@@ -1,12 +1,26 @@
 package com.berruhanedar.app.gym_springboot.entity;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
-@EqualsAndHashCode(callSuper = true)
+@EqualsAndHashCode(callSuper = true, exclude = {"trainees", "trainings"})
+@ToString(callSuper = true, exclude = {"trainees", "trainings"})
+@Entity
+@Table(name = "trainers")
 public class Trainer extends User {
-    private String specialization;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "specialization_id", nullable = false)
+    private TrainingType specialization;
+
+    @ManyToMany(mappedBy = "trainers")
+    private Set<Trainee> trainees = new HashSet<>();
+
+    @OneToMany(mappedBy = "trainer")
+    private Set<Training> trainings = new HashSet<>();
 }
