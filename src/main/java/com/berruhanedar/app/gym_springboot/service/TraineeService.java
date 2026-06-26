fb9d25128905809815a3fdc03e84ac156c00ad8d
Berru Hanedar <115636713+berruhanedar@users.noreply.github.com>
2026-06-26 23:51:15 +0300

    Trainee password change

--- src/main/java/com/berruhanedar/app/gym_springboot/service/TraineeService.java
+++ src/main/java/com/berruhanedar/app/gym_springboot/service/TraineeService.java
@@ -8,6 +8,7 @@ import com.berruhanedar.app.gym_springboot.mapper.TraineeMapper;
 import com.berruhanedar.app.gym_springboot.util.CredentialGenerator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -45,6 +46,7 @@ public class TraineeService {
         return traineeMapper.toDTO(saved);
     }
 
+    @Transactional
     public TraineeResponseDTO updateTrainee(UpdateTraineeRequestDTO dto) {
         log.info("Updating trainee profile. id={}", dto.getId());
         Trainee trainee = traineeDao.findById(dto.getId())
@@ -54,18 +56,21 @@ public class TraineeService {
         traineeMapper.updateFromDTO(dto, trainee);
         trainee.setUsername(existingUsername);
         trainee.setPassword(existingPassword);
-        Trainee updated = traineeDao.save(trainee);
+        Trainee updated = traineeDao.update(trainee);
         log.info("Trainee profile updated successfully. id={}", updated.getId());
         return traineeMapper.toDTO(updated);
     }
 
+    @Transactional
     public void deleteTrainee(Long id) {
         log.info("Deleting trainee profile. id={}", id);
-        traineeDao.findById(id).orElseThrow(() -> new EntityNotFoundException("Trainee not found. id=" + id));
-        traineeDao.delete(id);
+        Trainee trainee = traineeDao.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Trainee not found. id=" + id));
+        traineeDao.delete(trainee);
         log.info("Trainee profile deleted successfully. id={}", id);
     }
 
+    @Transactional(readOnly = true)
     public TraineeResponseDTO getTrainee(Long id) {
         log.debug("Selecting trainee profile. id={}", id);
         return traineeMapper.toDTO(traineeDao.findById(id)
@@ -80,5 +85,17 @@ public class TraineeService {
                         new EntityNotFoundException("Trainee not found: " + username));
 
         return traineeMapper.toDTO(trainee);
+    }
+
+    @Transactional
+    public void changePassword(String username, String newPassword) {
+
+        Trainee trainee = traineeDao.findByUsername(username)
+                .orElseThrow(() ->
+                        new EntityNotFoundException("Trainee not found."));
+
+        trainee.setPassword(newPassword);
+
+        traineeDao.update(trainee);
     }
 }
