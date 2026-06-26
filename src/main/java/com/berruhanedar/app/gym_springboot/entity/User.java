2476f93bf3bc4e274271b880a30b77532c0226d1
Berru Hanedar <115636713+berruhanedar@users.noreply.github.com>
2026-06-26 15:34:26 +0300

    Convert User to JPA entity

--- src/main/java/com/berruhanedar/app/gym_springboot/entity/User.java
+++ src/main/java/com/berruhanedar/app/gym_springboot/entity/User.java
@@ -1,17 +1,34 @@
 package com.berruhanedar.app.gym_springboot.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "users")
+@Inheritance(strategy = InheritanceType.JOINED)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public abstract class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "first_name", nullable = false)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false)
     private String lastName;
+
+    @Column(nullable = false, unique = true)
     private String username;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(name = "is_active", nullable = false)
     private Boolean isActive;
 }
