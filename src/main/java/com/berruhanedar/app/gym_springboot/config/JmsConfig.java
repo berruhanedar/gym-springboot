57b6fde28c2097b0fb816c971e0a5a0ddfba4131
Berru Hanedar <115636713+berruhanedar@users.noreply.github.com>
2026-08-14 21:40:10 +0300

    Fix JMS JSON type mapping between microservices

--- src/main/java/com/berruhanedar/app/gym_springboot/config/JmsConfig.java
+++ src/main/java/com/berruhanedar/app/gym_springboot/config/JmsConfig.java
@@ -1,5 +1,6 @@
 package com.berruhanedar.app.gym_springboot.config;
 
+import com.berruhanedar.app.gym_springboot.dto.TrainerWorkloadRequestDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,17 +8,18 @@ import org.springframework.jms.support.converter.MappingJackson2MessageConverter
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
 
+import java.util.Map;
+
 @Configuration
 public class JmsConfig {
 
     @Bean
     public MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper) {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-
         converter.setObjectMapper(objectMapper);
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
-
+        converter.setTypeIdMappings(Map.of("trainerWorkload", TrainerWorkloadRequestDTO.class));
         return converter;
     }
 }
