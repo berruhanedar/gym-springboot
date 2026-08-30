fc02950f6a5e7622a1c45bdd1209267426b61465
Berru Hanedar <115636713+berruhanedar@users.noreply.github.com>
2026-08-30 23:02:07 +0300

    Configure Cucumber test infrastructure

---
+++ src/main/resources/feature/smoke.feature
@@ -0,0 +1,5 @@
+Feature: Gym application Cucumber setup
+
+  Scenario: Cucumber test framework is configured
+   Given the Gym application test environment is available
+   Then Cucumber should run successfully
---
+++ src/test/java/com/berruhanedar/app/gym_springboot/cucumber/CucumberSpringConfiguration.java
@@ -0,0 +1,9 @@
+package com.berruhanedar.app.gym_springboot.cucumber;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@CucumberContextConfiguration
+@SpringBootTest
+public class CucumberSpringConfiguration {
+}
